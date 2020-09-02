798a2cd25347d3951df0548031d020d238ef725c
TannerLarson <tanner.b.larson@gmail.com>
2020-09-02 13:06:12 -0600

    More basics added to base python game model

--- cards.py
+++ cards.py
@@ -6,12 +6,91 @@ class Card():
 		self.description = data['description']
 		if 'execute' in data:
 			self.execute = data['execute']
+		if 'draw' in data:
+			self.draw = data['draw']
+		if 'action' in data:
+			self.action = data['action']
+		if 'buy' in data:
+			self.buy = data['buy']
+
+def print_action_details(card_name):
+	if card_name in dictionary:
+		print('\033[96m') # Make the card cyan
+		print("Name: {}".format(action[card_name]['name']))
+		print("Cost: {}".format(action[card_name]['cost']))
+		print("Description: {}".format(action[card_name]['description']))
+
+		# End coloring
+		print('\033[0m')
+	else:
+		print("Card not found!")
+	print()
+
+def get_type(card_key):
+	if card_key in dictionary:
+		return dictionary[card_key]['type']
+	else:
+		print("Card not found")
+		return None
 
 
+####################################################################
+# CARD DICTIONARIES
+# - Each name must be unique, including across different card types
+####################################################################
+
+treasure = {
+	
+	'copper': {
+		'name': "Copper",
+		'cost': 0,
+		'value': 1
+	},
+
+	'silver': {
+		'name': "Silver",
+		'cost': 3,
+		'value': 2
+	},
+
+	'gold': {
+		'name': "Gold",
+		'cost': 6,
+		'value': 3
+	},
+}
+
+victory = {
+	
+	'estate': {
+		'name': "Estate",
+		'cost': 2,
+		'points': 1
+	},
+
+	'dutchy': {
+		'name': "Dutchy",
+		'cost': 5,
+		'points': 3
+	},
+
+	'province': {
+		'name': "Province",
+		'cost': 8,
+		'points': 6
+	},
+
+	'curse': {
+		'name': "Curse",
+		'cost': 0,
+		'points': -1
+	}
+}
 
 
-###################################################################
-###################################################################
+####################################################################
+# Base
+####################################################################
 
 action = {
 
@@ -98,7 +177,7 @@ action = {
 		'name': "Library",
 		'cost': 5,
 		'description': "Draw until you have 7 cards in hand, skipping any"
-		"Action cards you choose to; set those aside, discarding them"
+		"Action cards you choose to; set those aside, discarding them "
 		"afterwards",
 		'execute': ''
 	},
@@ -235,52 +314,301 @@ action = {
 	}
 }
 
-###################################################################
 
-treasure = {
+dictionary = {
+
+	'artisan': {
+		'type': 'action',
+		'name': "Artisan",
+		'cost': 6,
+		'description': "Gain a card to your hand costing up to 5"
+			" treasure.\nPut a card from your hand onto your deck.",
+		'execute': ''
+	},
+
+	'bandit': {
+		'type': 'action',
+		'name': "Bandit",
+		'cost': 5,
+		'description': "Gain a Gold\n\
+			Each other player reveals the top 2 cards of their deck, "
+			"trashes a revealed Treasure other than Copper, and discards"
+			"the rest.",
+		'execute': ''
+	},
+
+	'bureaucrat': {
+		'type': 'action',
+		'name': "Bureaucrat",
+		'cost': 4,
+		'description': "Gain a silver onto your deck.\nEach other "
+			"player reveals a Victory card from their hand and puts it onto "
+			"their deck (or reveals a hand with no Victory cards).",
+		'execute': ''
+	},
+
+	'cellar': {
+		'type': 'action',
+		'name': "Cellar",
+		'cost': 2,
+		'description': "Discard any number of cards, then draw that many.",
+		'execute': '',
+		'action': 1
+	},
+
+	'chapel': {
+		'type': 'action',
+		'name': "Chapel",
+		'cost': 2,
+		'description': "Trash up to 4 cards from your hand.",
+		'execute': ''
+	},
+
+	'council_room': {
+		'type': 'action',
+		'name': "Council Room",
+		'cost': 5,
+		'description': "Each other player draws a card.",
+		'execute': '',
+		'draw': 4,
+		'buy': 1
+	},
+
+	'festival': {
+		'type': 'action',
+		'name': "Festival",
+		'cost': 5,
+		'description': "",
+		'action': 2,
+		'buy': 1,
+		'value': 2
+	},
+
+	'harbinger': {
+		'type': 'action',
+		'name': "Harbinger",
+		'cost': 3,
+		'description': "Look through your discard pile.\nYou may put a "
+		"card from it onto your deck.",
+		'execute': '',
+		'draw': 1,
+		'action': 1
+	},
+
+	'laboratory': {
+		'type': 'action',
+		'name': "Laboratory",
+		'cost': 5,
+		'description': "",
+		'execute': '',
+		'draw': 2,
+		'action': 1
+	},
+
+	'library': {
+		'type': 'action',
+		'name': "Library",
+		'cost': 5,
+		'description': "Draw until you have 7 cards in hand, skipping any"
+		"Action cards you choose to; set those aside, discarding them"
+		"afterwards",
+		'execute': ''
+	},
+
+	'market': {
+		'type': 'action',
+		'name': "Market",
+		'cost': 5,
+		'description': "",
+		'execute': '',
+		'draw': 1,
+		'action': 1,
+		'buy': 1,
+		'value': 1
+	},
+
+	'merchant': {
+		'type': 'action',
+		'name': "Merchant",
+		'cost': 3,
+		'description': "The first time you play a Silver this turn, "
+		"+1 Treasure",
+		'execute': '',
+		'draw': 1,
+		'action': 1
+	},
+
+	'militia': {
+		'type': 'action',
+		'name': "Militia",
+		'cost': 4,
+		'description': "Each other player discards down to 3 cards "
+		"in hand.",
+		'execute': '',
+		'value': 2
+	},
+
+	'mine': {
+		'type': 'action',
+		'name': "Mine",
+		'cost': 5,
+		'description': "You may trash a Treasure from your hand.  Gain"
+		" a Treasure to your hand costing up to 3 treasure more than"
+		" it.",
+		'execute': ''
+	},
+
+	'moat': {
+		'type': 'action',
+		'name': "Moat",
+		'cost': 2,
+		'description': "When another player plays an Attack card, you "
+		"may first reveal this from your hand, to be unaffected by it.",
+		'execute': '',
+		'draw': 2
+	},
+
+	'moneylender': {
+		'type': 'action',
+		'name': "Moneylender",
+		'cost': 4,
+		'description': "You may trash a Copper from your hand for +3"
+		" Treasure.",
+		'execute': ''
+	},
+
+	'poacher': {
+		'type': 'action',
+		'name': "Poacher",
+		'cost': 4,
+		'description': "Discard a card per empty Supply pile.",
+		'execute': '',
+		'draw': 1,
+		'action': 1,
+		'value': 1
+	},
+
+	'remodel': {
+		'type': 'action',
+		'name': "Remodel",
+		'cost': 4,
+		'description': "Trash a card from your hand. Gain a card "
+		"costing up to 2 Treasure more than it.",
+		'execute': ''
+	},
+
+	'sentry': {
+		'type': 'action',
+		'name': "Sentry",
+		'cost': 5,
+		'description': "Look at the top 2 cards of your deck.  Trash "
+		"and/or discard any number of them.  Put the rest back on top"
+		" in any order.",
+		'execute': ''
+	},
+
+	'smithy': {
+		'type': 'action',
+		'name': "Smithy",
+		'cost': 4,
+		'description': "",
+		'execute': '',
+		'draw': 3
+	},
+
+	'throne_room': {
+		'type': 'action',
+		'name': "Throne Room",
+		'cost': 4,
+		'description': "You may play an Action card from your hand twice.",
+		'execute': ''
+	},
+
+	'vassal': {
+		'type': 'action',
+		'name': "Vassal",
+		'cost': 3,
+		'description': "Discard the top card of your deck.  If it's an "
+		"Action card, you may play it.",
+		'execute': '',
+		'value': 2
+	},
+
+	'village': {
+		'type': 'action',
+		'name': "Village",
+		'cost': 3,
+		'description': "",
+		'execute': '',
+		'draw': 1,
+		'action': 2
+	},
+
+	'witch': {
+		'type': 'action',
+		'name': "Witch",
+		'cost': 5,
+		'description': "Each other player gains a Curse",
+		'execute': '',
+		'draw': 2
+	},
+
+	'workshop': {
+		'type': 'action',
+		'name': "Workshop",
+		'cost': 3,
+		'description': "Gain a card costing up to 4 Treasure",
+		'execute': ''
+	},
+
+	#### Treasure ##########################################
 	
-	'c': {
+	'copper': {
+		'type': 'treasure',
 		'name': "Copper",
 		'cost': 0,
 		'value': 1
 	},
 
-	's': {
+	'silver': {
+		'type': 'treasure',
 		'name': "Silver",
 		'cost': 3,
 		'value': 2
 	},
 
-	'g': {
+	'gold': {
+		'type': 'treasure',
 		'name': "Gold",
 		'cost': 6,
 		'value': 3
 	},
-}
 
-####################################################################
-
-victory = {
+	#### Victory #############################################3
 	
-	'e': {
+	'estate': {
+		'type': 'victory',
 		'name': "Estate",
 		'cost': 2,
 		'points': 1
 	},
 
-	'd': {
+	'dutchy': {
+		'type': 'victory',
 		'name': "Dutchy",
 		'cost': 5,
 		'points': 3
 	},
 
-	'p': {
+	'province': {
+		'type': 'victory',
 		'name': "Province",
 		'cost': 8,
 		'points': 6
 	},
 
-	'c': {
+	'curse': {
+		'type': 'victory',
 		'name': "Curse",
 		'cost': 0,
 		'points': -1
--- dominion.py
+++ dominion.py
@@ -1,28 +1,103 @@
+'''
+IDEAS TO ADD:
+Small:
+	Undo option
+Big:
+	AI player
+	GUI
+
+'''
+
+
+
+
+
+
 import cards
 import random 
+import math
+import copy
+
+######################################
+# Add color and bold/underline to text
+######################################
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
 
 class Board():
 
 	def __init__(self, num_players):
-		self.num_estates
-		self.num_dutchies
-		self.num_provinces
-		self.num_curses
-		self.num_copper
-		self.num_silver
-		self.num_gold
-		self.action_cards = {}
+		num_victory_cards = 4 + math.ceil(float(num_players) / 2) * 4
+		self.num_estates = num_victory_cards
+		self.num_dutchies = num_victory_cards
+		self.num_provinces = num_victory_cards
+		self.num_curses = (num_players - 1) * 10
+		self.num_copper = 60 - 7 * num_players
+		self.num_silver = 40
+		self.num_gold = 30
+		self.action_cards = []
+		self.num_action_cards = [10 for x in range(10)]
+
+		# Get 10 random action cards for the board and sort them by price
+		action_list = random.sample(range(0, len(cards.action)), 10)
+		for x in action_list:
+			self.action_cards.append(cards.action[list(cards.action)[x]])
+		self.action_cards.sort(reverse=True, key=lambda e : e['cost'])
+
+	def display(self):
+		# Action Cards
+		print(color.BOLD + color.CYAN + 'Action Cards:' + color.END)
+		print(color.BOLD + color.BLUE + "Cost |     Card      | Left" + color.END)
+		i = 0
+		for card in self.action_cards:
+			print("{:^5}| {:^13} | {:^5}".format(card['cost'], card['name'], self.num_action_cards[i]))
+			i += 1
+		print()
+
+		# Treasure Cards
+		print(color.BOLD + color.YELLOW + 'Treasure Cards:' + color.END)
+		print(color.BOLD + color.BLUE + "Cost |  Card  | Value | Left" + color.END)
+		print("{:^5}| {:^6} | {:^5} | {:^5}".format(6, 'Gold', 3, self.num_gold))
+		print("{:^5}| {:^6} | {:^5} | {:^5}".format(3, 'Silver', 2, self.num_silver))
+		print("{:^5}| {:^6} | {:^5} | {:^5}".format(1, 'Copper', 0, self.num_copper))
+		print()
+
+
+		# Victory Cards
+		print(color.BOLD + color.GREEN + 'Victory Cards:' + color.END)
+		print(color.BOLD + color.BLUE + "Cost |     Card      | VP | Left" + color.END)
+		print("{:^5}| {:^13} | {:>2} | {:^5}".format(6, 'Province', 6, self.num_provinces))
+		print("{:^5}| {:^13} | {:>2} | {:^5}".format(6, 'Dutchy', 3, self.num_dutchies))
+		print("{:^5}| {:^13} | {:>2} | {:^5}".format(6, 'Estate', 1, self.num_estates))
+		print("{:^5}| {:^13} | {:^2} | {:^5}".format(6, 'Curse', -1, self.num_curses))
+		print()
+
 
 class Player():
 
 	def __init__(self):
 		self.discard_pile = []
-		self.draw_pile = random.sample(['c','c','c','c','c','c','c','e','e','e'], 10)
+		
+		# Shuffle the draw pile
+		self.draw_pile = random.sample(['copper','copper','copper','copper','copper', \
+			'copper','copper','estate','estate','estate'], 10)
 		self.num_actions = 1
 		self.num_buys = 1
 		self.hand = self.draw_pile[:5]
 
-	def shuffle_deck():
+		self.draw_pile = self.draw_pile[5:]
+
+	def shuffle():
 		pass
 
 	def draw(num = 1):
@@ -33,17 +108,116 @@ class Player():
 		self.hand = self.draw_pile[:num]
 		self.draw_pile = self.draw_pile[num:]
 
-	def buy():
+	def buy(self):
+		pass
+
+	def reset(self):
+		self.num_actions = 1
+		self.num_buys = 1
+		self.discard_hand
+		#self.draw(5)
+
+	def display_hand(self):
+		print("Hand:")
+		for card in self.hand:
+			card_actual = cards.dictionary[card]
+			if card_actual['type'] == 'action':
+				print(color.CYAN + card_actual['name'] + color.END)
+			elif card_actual['type'] == 'treasure':
+				print(color.YELLOW + card_actual['name'] + " ${}".format(card_actual['value']) + color.END)
+			elif card_actual['type'] == 'victory':
+				print(color.GREEN + card_actual['name'] + color.END)
+			else:
+				print(card_actual['name'])
+		print()
+
+	def discard_hand(self):
 		pass
 
 
+
 def main():
-    num_players = int(input("Number of players: "))
-    if num_players < 2:
-    	print("Not enough players")
-    	return
-    players = [Player() for x in range(0, num_players)]
-    print(players[0].hand)
+
+	# Set up game
+	num_players = 2 #int(input("Number of players: "))
+	if num_players < 2:
+		print("Not enough players")
+		return
+	elif num_players > 4:
+		print("Too many players")
+		return
+	players = [Player() for x in range(0, num_players)]
+	#print(players[0].hand)
+
+	board = Board(num_players)
+
+	# Main game loop
+	game_done = False
+	play_order = random.sample([x for x in range(1,num_players+1)], num_players)
+	i_play_order = 0
+	while not game_done:
+		board.display()
+		print(color.RED + "Player {}'s turn".format(play_order[i_play_order]) + color.END)
+		current_player = players[play_order[i_play_order]-1]
+		current_player.display_hand()
+
+		# Action phase
+		while 1:
+			choice = None
+			while choice is None:
+				try:
+					choice = int(input('Choose an option:\n1. Show board\n2. Read action card description\n3. Use action ({} actions left)\n4. End action phase\n>> ' \
+						.format(current_player.num_actions)))
+					if not 0 < choice < 5:
+						raise ValueError()
+				except ValueError:
+					print("ERROR: Please input a number between 1 and 5: ")
+
+			if choice == 1:
+				board.display()
+			elif choice == 2:
+				card_to_read = input("Name of action card: ").lower()
+				cards.print_action_details(card_to_read)
+			elif choice == 3:
+				pass
+			elif choice == 4:
+				break
+
+		# Buy phase
+		while 1:
+			choice = int(input('Choose an option:\n1. Show board\n2. Read action card description\n3. Buy card ({} buys left)\n4. End turn\n>> ' \
+				.format(current_player.num_buys)))
+			while not 0 < choice < 5:
+				choice = input("Please input a number: ")
+
+			if choice == 1:
+				board.display()
+			elif choice == 2:
+				card_to_read = input("Name of action card: ").lower()
+				cards.print_action_details(card_to_read)
+			elif choice == 3:
+				if current_player.num_buys > 0:
+					# Calculate total money in hand
+					total_money = 0
+					for card_key in current_player.hand:
+						if cards.get_type(card_key) == 'treasure':
+							total_money += cards.treasure[card_key]['value']
+
+					to_buy = input("Card to buy: ")
+					if to_buy in cards.dictionary:
+						pass
+					else:
+						print("Card not found")
+				else:
+					print("No buys left!")
+			elif choice == 4:
+				break
+
+		# Clean up
+		current_player.reset()
+		i_play_order += 1
+
+		#game_done = True
 
 if __name__ == "__main__":
     main()
